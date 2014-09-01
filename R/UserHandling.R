9d94e59a4a548782a100a2ddb128316865c53aa1
gcorre02 <Guilherme Ribeiro>
2014-09-01 13:14:27 +0100

    added css support. addded submit button and search box for tickers and company names

--- R/UserHandling.R
+++ R/UserHandling.R
@@ -1,20 +1,25 @@
-load("data/logins.Rda", envir = as.environment(".GlobalEnv"))
+#library(opencpu)
+l1 = new.env(hash = TRUE, parent = parent.frame(), size = 29L)
+load("data/logins.Rda",  envir = l1)
 
 persistData = function(logins, filename){
-  target = paste0("data/", filename,".Rda")
+  target = paste0("/Users/gctribeiro/test3/data/", filename,".Rda")#this must be solved on the server itself., how do you access session variables?
  # logins = data
-  unlockBinding("logins", as.environment(".GlobalEnv"))
-  assign("logins", logins, envir = as.environment(".GlobalEnv")) 
-  lockBinding("logins", as.environment(".GlobalEnv"))
+  unlockBinding("logins", l1)
+  assign("logins", logins, envir = l1) 
+  lockBinding("logins", l1)
   save(logins, file = target)
   #saveRDS(data,target)
 }
 
 addUser = function(username, password){
-  loginsFile = logins#readRDS(file="data/logins.Rda")
+  unlockBinding("logins", l1)
+  load("/Users/gctribeiro/test3/data/logins.Rda",  envir = l1)
+  
+  loginsFile = l1$logins#readRDS(file="data/logins.Rda")
   if(is.na(loginsFile[username,])){
-  loginsFile = rbind(loginsFile, as.data.frame(password,row.names=username, col.names= "password"))
-  persistData(loginsFile,"logins")
+    loginsFile = rbind(loginsFile, as.data.frame(password,row.names=username, col.names= "password"))
+    persistData(loginsFile,"logins")
     return("successful")
   }
   #add trycatch with return of success or notelse error !
@@ -28,10 +33,13 @@ createNewUserDB = function(){
 }
 
 checkLogin = function(user, pass){
-  loginsFile = logins #readRDS(file="data/logins.Rda")
+  unlockBinding("logins", l1)
+  load("/Users/gctribeiro/test3/data/logins.Rda",  envir = l1)
+  
+  loginsFile = l1$logins #readRDS(file="data/logins.Rda")
   
   if(is.na(loginsFile[user,])){
-    return("unregistered")
+    return("Unregistered")
     #throw error, ask for registering
   } else {
     if(loginsFile[user,]!=pass){
--- inst/www/stocksSelect.js
+++ inst/www/stocksSelect.js
@@ -1,28 +1,79 @@
-
-function generateStocksList() {
+//edit this function to work both for tickers and stock names
+//attention: forms reload!!!, that might be interesting ?
+function generateStocksList(listType, formId,target) {//got this: from somewhere! w3 school thingy ? 
       var x = document.createElement("INPUT");
-      x.setAttribute("list", "tickers");
-      document.getElementById("securityList").appendChild(x);
+      x.setAttribute("list", target);
+      x.setAttribute("id", "input_" + target);
+      document.getElementById(formId).appendChild(x);
   
       var y = document.createElement("DATALIST");
-      y.setAttribute("id", "tickers");
-      document.getElementById("securityList").appendChild(y);
-  
-      var z = document.createElement("OPTION");
-      z.setAttribute("value", "Chrome");
-      document.getElementById("tickers").appendChild(z);
+      y.setAttribute("id", target);
+      document.getElementById(formId).appendChild(y);
       
-      appendOptions("awesome", "tickers");
-      ocpu.rpc("getTickers",{
+      var numBox = document.createElement("INPUT");
+      numBox.setAttribute("id", "numbox");
+      document.getElementById(formId).appendChild(numbox);
+
+      
+      var button = document.createElement("BUTTON");
+      button.setAttribute("id", ("submit"+target));
+      var btnText = document.createTextNode("Submit");
+      button.appendChild(btnText);
+      document.getElementById(formId).appendChild(button);
+      
+      appendOptions("awesome", target);
+      //the list type directly calls the right R method
+      ocpu.rpc(listType,{
       //args?
       },function(output){
         //something = ("ewrdad,aerwefsef,aer,ar,ad");
         //alert( typeof String(output).split(','));
-        
+
         allTickers = String(output).split(',');
         for(tcker in allTickers){
-          appendOptions(allTickers[tcker], "tickers");      
+          appendOptions(allTickers[tcker], target);      
         }
+      });
+      
+      $("#"+"submit"+target).on("click", function(){
+        //disable the button to prevent multiple clicks
+        $("#"+"submit"+target).attr("disabled", "disabled");
+        var choice = $("#"+"input_" + target).val();
+        var percentage = $("#numbox").val();
+       // var regpass = $("#regPass").val();
+        alert("Submit button pressed, choice: " + choice + " for a weight of: "+ percentage + "%");
+        
+        //have form
+        //update/populate form
+        //submit form to R!!
+        
+        /*
+        var validateRegister = ocpu.rpc("addUser", {
+          username : reguser,
+          password : regpass
+        }, function(output){
+          //should link somewhere
+          alert("Your registration was " + output) ;/*
+          if(output == "true"){//note : only == works here...
+            $("#login").text("Login Successful");
+          } else if (output == "false"){
+            $("#login").text("Login unsuccessful");
+          } else {
+            $("#login").text("Please Register");
+            //call function to generate user registration
+            addInputMethod();
+          }
+        }).fail(function(){
+          alert("R failed " + validateLogin.responseText)
+        });*/
+        
+        $("#"+"submit"+target).removeAttr("disabled");
+        //create a function for this:
+        document.getElementById(formId).removeChild(x);
+        document.getElementById(formId).removeChild(y);
+        document.getElementById(formId).removeChild(button);
+        document.getElementById(formId).removeChild(numbox);
+
       });
 }
 
